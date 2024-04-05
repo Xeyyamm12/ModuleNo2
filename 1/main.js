0dd8440a61555db53cbeb9b84b3656e562dccc57
Xeyyam <Xeyyam@Xeyyam>
2024-04-05 16:19:11 +0300

    2 task

--- 1/main.js
+++
@@ -1,6 +0,0 @@
-//1
-// const sentence = [`sense.`, `make`, `all`, `will`, `This`];
-// function reverseArray(arr) {
-//     return arr.reverse();
-// }
-// console.log(reverseArray(sentence))
---
+++ main.js
@@ -0,0 +1,33 @@
+//1
+// const sentence = [`sense.`, `make`, `all`, `will`, `This`];
+// function reverseArray(arr) {
+//     return arr.reverse();
+// }
+// console.log(reverseArray(sentence))
+//3.2
+// const coolStuff = [
+//     'gameboys',
+//     'skateboards',
+//     'backwards hats',
+//     'my room',
+//     'temporary tattoos'
+//   ];
+//   const myStuff = [
+//     'sweaters',
+//     'skateboards',
+//     'family-night',
+//     'my room',
+//   ];
+//   let a=[];
+//   function justCoolStuff(arr1,arr2){
+  
+//       for(let i=0;i<arr1.length;i++){
+//            for(let j=0;j<arr2.length;j++){
+//           if(arr1[i]===arr2[j]){
+//               a.push(arr2[j]);
+//           }
+//       }
+//       }
+//       return a
+//   }
+//   console.log(justCoolStuff(myStuff, coolStuff));
