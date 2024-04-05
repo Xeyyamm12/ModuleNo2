bc799823478e7be9bbab7990590fd6129789b58e
Xeyyam <Xeyyam@Xeyyam>
2024-04-05 16:21:57 +0300

    task 2

--- main.js
+++ main.js
@@ -4,6 +4,43 @@
 //     return arr.reverse();
 // }
 // console.log(reverseArray(sentence))
+//2
+// const greetAliens = function(aliens){
+//     aliens.forEach(name => {
+//       console.log(Oh powerful ${name}, we humans offer our unconditional surrender!)
+//     });
+  
+//   }
+//   const aliens = ['Blorgous', 'Glamyx', 'Wegord','SpaceKing'];
+//   greetAliens(aliens);
+  
+  
+  
+//   const meal = [
+//     { name: 'arugula', source: 'plant' },
+//     { name: 'tomatoes', source: 'plant' },
+//     { name: 'lemon', source: 'plant' },
+//     { name: 'olive oil', source: 'plant' },
+//   ];
+//   const dinner = [
+//     { name: 'hamburger', source: 'meat' },
+//     { name: 'cheese', source: 'dairy' },
+//     { name: 'ketchup', source: 'plant' },
+//     { name: 'bun', source: 'plant' }
+//   ];
+  
+//   const isTheDinnerVegan  = function(food){
+//         food.forEach(item=>{
+//         // console.log(item.source)
+//         if(item.source === 'plant'){
+//           console.log(true)
+//         }
+//         else console.log(false)
+//       })
+//   }
+  
+//   console.log(isTheDinnerVegan(meal));
+//   console.log(isTheDinnerVegan(dinner));
 //3.2
 // const coolStuff = [
 //     'gameboys',
